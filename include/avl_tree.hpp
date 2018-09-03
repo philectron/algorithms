c18eea0ceec6b0293200cfa1cd477f71e79c5656
Phi Luu <philectron@users.noreply.github.com>
2018-09-03 16:12:37 -0700

    Implement Insert() and Print() methods

--- include/avl_tree.hpp
+++ include/avl_tree.hpp
@@ -6,20 +6,22 @@
 //
 // This template class serves as a way to practice my data structure skills.
 // This class provides methods to interact with an AVL tree, including
-// - Creating the tree
-// - Making a copy of the tree
-// - Find maximum and minimum elements in the tree
-// - Checking whether an element is in the tree
-// - Adding an element to the tree
-// - Removing an element from the tree
-// - Clearing the tree
+//     - Creating the tree
+//     - Making a copy of the tree
+//     - Find maximum and minimum elements in the tree
+//     - Checking whether an element is in the tree
+//     - Adding an element to the tree
+//     - Removing an element from the tree
+//     - Clearing the tree
 
 #ifndef ALGORITHMS_INCLUDE_AVLTREE_HPP_
 #define ALGORITHMS_INCLUDE_AVLTREE_HPP_
 
 #include <iostream>
+#include <string>
 #include <utility>
 
+using std::endl;
 using std::ostream;
 
 template <class Comparable>
@@ -33,8 +35,8 @@ public:
 
     AvlTree(AvlTree&& rhs)
         : root_{rhs.root_}, size_{rhs.size_}, height_{rhs.height_} {
-        rhs.root_ = nullptr;
-        rhs.size_ = rhs.height_ = 0;
+          rhs.root_ = nullptr;
+          rhs.size_ = rhs.height_ = 0;
     }
 
     AvlTree& operator=(const AvlTree& rhs) {
@@ -70,15 +72,18 @@ public:
 
     bool Contains(const Comparable& object) const { return false; }
 
-    void Insert(const Comparable& object) {}
-
-    void Insert(Comparable&& object) {}
+    void Insert(const Comparable& object) {
+        InsertNode(object, root_);
+        size_++;
+        height_ = Height(root_) + 1;
+    }
 
     void Remove(const Comparable& object) {}
 
     void RemoveAll(const Comparable& object) {}
 
     friend ostream& operator<<(ostream& out, const AvlTree& avltree) {
+        avltree.Print(out, avltree.root_);
         return out;
     }
 
@@ -134,8 +139,167 @@ private:
     void DeleteNode(AvlNode* node) {
         if (node) {
             DeleteNode(node->left);
-            Deletenode(node->right);
+            DeleteNode(node->right);
             delete node;
+        }
+    }
+
+    // Internal recursive method
+    // Inserts a new object into the subtree with root  node .
+    // Also, balances the new subtree and sets its new root.
+    void InsertNode(const Comparable& object, AvlNode*& node) {
+        // if reach where the new node needs to be inserted into
+        if (!node)
+            node = new AvlNode(object);
+        // if the object is less than current node, go left
+        else if (object < node->data)
+            InsertNode(object, node->left);
+        // if the object is more than current node, go right
+        else if (object > node->data)
+            InsertNode(object, node->right);
+
+        // balance the subtree from bottom to top
+        Balance(node);
+    }
+
+    // If the difference in heights of two sibling nodes is 1 or less, then
+    // it is an acceptable imbalance factor.
+    static const int ALLOWED_IMBALANCE = 1;
+
+    // Internal method
+    // Balances the AVL subtree with root  node  using the acceptable imbalance
+    // factor above.
+    void Balance(AvlNode*& node) {
+        if (!node) return;
+
+        // if subtree is heavy on the left
+        if (Height(node->left) - Height(node->right) > ALLOWED_IMBALANCE) {
+            // if the left child is heavy on the right, perform double rotation
+            if (Height(node->left->left) < Height(node->left->right))
+                RotateLeft(node->left);
+            // still need to rotate right regardless of double rotation
+            RotateRight(node);
+        }
+        // if subtree is heavy on the right
+        else if (Height(node->right) - Height(node->left) > ALLOWED_IMBALANCE) {
+            // if the right child is heavy on the left, perform double rotation
+            if (Height(node->right->right) < Height(node->right->left))
+                RotateRight(node->right);
+            // still need to rotate left regardless of double rotation
+            RotateLeft(node);
+        }
+
+        // set new height for the root of subtree
+        SetHeight(node);
+    }
+
+    // Internal method
+    // Rotates subtree to the left and sets its new root.
+    void RotateLeft(AvlNode*& node) {
+        AvlNode* new_root = node->right;
+
+        node->right = new_root->left;
+        new_root->left = node;
+        SetHeight(node);
+        SetHeight(new_root);
+        node = new_root;
+    }
+
+    // Internal method
+    // Rotates subtree to the right and sets its new root.
+    void RotateRight(AvlNode*& node) {
+        AvlNode* new_root = node->left;
+
+        node->left = new_root->right;
+        new_root->right = node;
+        SetHeight(node);
+        SetHeight(new_root);
+        node = new_root;
+    }
+
+    // Internal method
+    // Returns the height of  node  or -1 if nullptr.
+    int Height(AvlNode* node) const {
+        if (node)
+            return node->height;
+        else
+            return -1;
+    }
+
+    // Internal method
+    // Sets the height of  node  when its children change.
+    void SetHeight(AvlNode*& node) {
+        int left_height = Height(node->left);
+        int right_height = Height(node->right);
+
+        if (left_height > right_height)
+            node->height = left_height + 1;
+        else
+            node->height = right_height + 1;
+    }
+
+    // // Internal recursive method
+    // // Out streams all nodes of an AVL tree (Ugly version) in preorder.
+    // // Node format: parentNodeVal_NodeVal
+    // void UglyPrint(ostream& out, AvlNode* node,
+    //                AvlNode* parent = nullptr) const {
+    //     if (!node) return;
+
+    //     if (parent) out << parent->data << '_';
+    //     out << node->data << ' ';
+    //     UglyPrint(out, node->left, node);
+    //     UglyPrint(out, node->right, node);
+    // }
+
+    // Internal method
+    // Out streams all nodes of an AVL tree.
+    // The direction of the tree is left to right (instead of up to down).
+    // Credits: https://stackoverflow.com/a/19484210/4048938
+    void Print(ostream& out, AvlNode* root) const {
+        if (!root) {
+            out << "Tree is empty" << endl;
+        } else {
+            // print all the nodes to the right of the root of this subtree
+            if (root->right) PrintNode(out, root->right, true, "");
+            // print the root of this subtree
+            out << root->data << endl;
+            // print all the nodes to the left of the root of this subtree
+            if (root->left) PrintNode(out, root->left, false, "");
+        }
+    }
+
+    // Internal recursive method
+    // Out streams all nodes of a subtree with root  node .
+    // The direction of the tree is left to right (instead of up to down).
+    // Credits: https://stackoverflow.com/a/19484210/4048938
+    void PrintNode(ostream& out, AvlNode* node, bool is_right_node,
+                   std::string indent) const {
+        // print the right nodes until the rightmost
+        if (node->right) {
+            if (is_right_node) {
+                PrintNode(out, node->right, true, indent + "        ");
+            } else {
+                PrintNode(out, node->right, true, indent + " |      ");
+            }
+        }
+
+        out << indent;
+
+        // different branching symbol for left and right nodes
+        if (is_right_node)
+            out << " /";
+        else
+            out << " \\";
+
+        out << "----- " << node->data << endl;
+
+        // print the left nodes until the leftmost
+        if (node->left) {
+            if (is_right_node) {
+                PrintNode(out, node->left, false, indent + " |      ");
+            } else {
+                PrintNode(out, node->left, false, indent + "        ");
+            }
         }
     }
 };
