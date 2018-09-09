233f5647e443dabbea2d6852d9e71b08a2c985b2
Phi Luu <philectron@users.noreply.github.com>
2018-09-08 17:30:11 -0700

    Implement debugging print methods

--- include/binary_heap.hpp
+++ include/binary_heap.hpp
@@ -46,13 +46,72 @@ public:
 
     static const int DEFAULT_CAPACITY = 32;
 
+    // For debugging purposes
+    friend ostream& operator<<(ostream& out, const BinaryHeap& heap) {
+        if (heap.IsEmpty()) {
+            out << "Binary heap is empty" << endl;
+        } else {
+            out << "Size = " << heap.Size() << endl;
+            // print the right nodes. The smallest index of right nodes is 2
+            if (heap.size_ >= 3) heap.Print(out, heap.array_, 2, true, "");
+            // print the root of the heap (index 0)
+            out << heap.array_.front() << endl;
+            // print the left nodes. The smallest index of left nodes is 1
+            if (heap.size_ >= 2) heap.Print(out, heap.array_, 1, false, "");
+        }
+
+        return out;
+    }
+
 private:
+    // children at index  i  are stored at indices
+    // 2i + 1  (left child) and  2i + 2  (right child)
     std::vector<Comparable> array_;
     int size_;
 
     void BuildHeap() {}
 
     void PercolateDown(int hole) {}
+
+    // Internal recursive method
+    // Out streams all nodes of a heap (dynamic array form) starting from the
+    // start_idx .
+    // The direction of the heap is left to right (instead of up to down).
+    // Credits: https://stackoverflow.com/a/19484210/4048938
+    void Print(ostream& out, const std::vector<Comparable>& array,
+               int start_idx, bool is_right_node, std::string indent) const {
+        // start_idx  should be and must be greater than zero and less than
+        // array's size
+        assert(start_idx > 0 && start_idx < (int)array.size());
+
+        // print right children first until the right most
+        if (start_idx * 2 + 2 < (int)array.size()) {
+            if (is_right_node) {
+                Print(out, array, start_idx * 2 + 2, true, indent + "        ");
+            } else {
+                Print(out, array, start_idx * 2 + 2, true, indent + " |      ");
+            }
+        }
+
+        out << indent;
+
+        // different branching symbol for left and right nodes
+        if (is_right_node) out << " /";
+        else out << " \\";
+
+        out << "----- " << array[start_idx] << endl;
+
+        // print left children until the leftmost
+        if (start_idx * 2 + 1 < (int)array.size()) {
+            if (is_right_node) {
+                Print(out, array, start_idx * 2 + 1, false,
+                      indent + " |      ");
+            } else {
+                Print(out, array, start_idx * 2 + 1, false,
+                      indent + "        ");
+            }
+        }
+    }
 };
 
 #endif  // ALGORITHMS_INCLUDE_BINARYHEAP_HPP_
--- src/binary_heap.cpp
+++ src/binary_heap.cpp
@@ -7,6 +7,32 @@
 using std::endl;
 
 int main() {
+    // <QUICK TEST>
+    std::vector<int> vector;
+    vector.push_back(13);
+    vector.push_back(21);
+    vector.push_back(16);
+    vector.push_back(24);
+    vector.push_back(31);
+    vector.push_back(19);
+    vector.push_back(68);
+    vector.push_back(65);
+    vector.push_back(26);
+    vector.push_back(32);
+    BinaryHeap<int> heap(vector);
+    BinaryHeap<int> heap2;
+    BinaryHeap<int> heap3 = heap;
+    heap3 = heap3;
+    heap2 = heap3;
+
+    std::vector<int> v1 = heap2.ToVector();
+    for (const auto& object : v1) std::cout << object << ' ';
+    std::cout << endl;
+
+    std::cout << heap2 << endl;
+
+    return 0;
+    // </QUICK TEST>
     std::ifstream fin("../input/binary_heap.in");
     std::ofstream fou("../output/binary_heap.ou");
 
