3a98b35f1236da9329e8399d337597164658a011
Phi Luu <philectron@users.noreply.github.com>
2018-09-13 18:21:20 -0700

    Change type name in bubble sort

--- include/bubble_sort.hpp
+++ include/bubble_sort.hpp
@@ -19,44 +19,13 @@
 namespace algorithm {
 
 // A BubbleSort version used for normal arrays.
-template<class Comparable>
-void BubbleSort(Comparable* array, int size) {
-    // flag to check swapped elements, first assuming the array is unsorted
-    bool swapped = true;
-    Comparable tmp(0);
+template <class Comparable>
+void BubbleSort(Comparable* array, size_t size) {
+    // flag to check swapped elements
+    bool swapped;
 
     // keep sorting while there are unorder pairs
-    while (swapped) {
-        // assume there is no out-of-order pairs
-        swapped = false;
-
-        // for each adjacent pair, swap the out-of-order ones (if any)
-        for (int i = 0; i < size - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                // perform the swap
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                // swap performed means there is an unordered pair
-                swapped = true;
-            }
-        }
-
-        // the largest value is at the right most of the array
-        // so decrease the size of the array that needs to be sorted
-        size--;
-    }
-}
-
-// A BubbleSort version used for smart arrays, such as vectors.
-template<class Comparable>
-void BubbleSort(Comparable& array) {
-    // flag to check swapped elements, first assuming the array is unsorted
-    bool swapped = true;
-    size_t size = array.size();
-
-    // keep sorting while there are unorder pairs
-    while (swapped) {
+    do {
         // assume there is no out-of-order pairs
         swapped = false;
 
@@ -71,7 +40,33 @@ void BubbleSort(Comparable& array) {
         // the largest value is at the right most of the array
         // so decrease the size of the array that needs to be sorted
         size--;
-    }
+    } while (swapped);
+}
+
+// A BubbleSort version used for smart arrays, such as vectors.
+template<class ArrayType>
+void BubbleSort(ArrayType& array) {
+    // flag to check swapped elements
+    bool swapped;
+    size_t size = array.size();
+
+    // keep sorting while there are unorder pairs
+    do {
+        // assume there is no out-of-order pairs
+        swapped = false;
+
+        // for each adjacent pair, swap the out-of-order ones (if any)
+        for (size_t i = 0; i < size - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                std::swap(array[i], array[i + 1]);
+                swapped = true;
+            }
+        }
+
+        // the largest value is at the right most of the array
+        // so decrease the size of the array that needs to be sorted
+        size--;
+    } while (swapped);
 }
 
 }  // namespace algorithm
