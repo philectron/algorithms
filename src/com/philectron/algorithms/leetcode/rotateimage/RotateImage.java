d3c6a2c6a5007b5dba6f408d95dea2dbc576c99b
Phi Luu <philectron@users.noreply.github.com>
2024-08-08 16:34:19 -0700

    Add a better solution to Rotate Image

--- src/com/philectron/algorithms/leetcode/rotateimage/RotateImage.java
+++ src/com/philectron/algorithms/leetcode/rotateimage/RotateImage.java
@@ -5,7 +5,33 @@ package com.philectron.algorithms.leetcode.rotateimage;
  */
 public class RotateImage {
 
-    public int[][] rotateExceptDiagonals(final int[][] matrix) {
+    public int[][] rotateClockwise(final int[][] matrix) {
+        for (int i = 0, n = matrix.length; i < n; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                rotateCellsClockwise(matrix, i, j);
+            }
+        }
+        return matrix;
+    }
+
+    public int[][] rotateClockwiseExceptDiagonalsWithLooping(final int[][] matrix) {
+        // This is a much shorter solution compared to using 4 different indices.
+        // Loop over the top quarter only since we can get the indices of the corresponding cells
+        // of the other quarters by rotating the top indices.
+        for (int i = 0, n = matrix.length; i < n; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                // Skip if the cell is on the principal diagonal.
+                if (i == j) {
+                    continue;
+                }
+                rotateCellsClockwise(matrix, i, j);
+            }
+        }
+
+        return matrix;
+    }
+
+    public int[][] rotateClockwiseExceptDiagonalsWithIndexing(final int[][] matrix) {
         final int n = matrix.length;
 
         // Use 4 cursors, one for each quarter.
@@ -59,6 +85,20 @@ public class RotateImage {
         final int tmp = matrix[i][j];
         matrix[i][j] = matrix[k][l];
         matrix[k][l] = tmp;
+    }
+
+    private void rotateCellsClockwise(final int[][] matrix, final int i, final int j) {
+        final int n = matrix.length;
+        // Temporarily save the top quarter's value.
+        final int tmp = matrix[i][j];
+        // Rotate left to top.
+        matrix[i][j] = matrix[n - 1 - j][i];
+        // Rotate bottom to left.
+        matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+        // Rotate right to bottom.
+        matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+        // Rotate top to right.
+        matrix[j][n - 1 - i] = tmp;
     }
 
 }
--- tests/com/philectron/algorithms/leetcode/rotateimage/RotateImageTest.java
+++ tests/com/philectron/algorithms/leetcode/rotateimage/RotateImageTest.java
@@ -10,54 +10,134 @@ public class RotateImageTest {
     private static final RotateImage solution = new RotateImage();
 
     @Test
-    public void rotateExceptDiagonals_0x0() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(0));
-        assertSquareMatricesEqual(actual, new int[0][0]);
+    public void rotateClockwise_0x0() {
+        final int n = 0;
+        final int[][] expected = new int[0][0];
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
     }
 
     @Test
-    public void rotateExceptDiagonals_1x1() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(1));
+    public void rotateClockwiseExceptDiagonals_0x0() {
+        final int n = 0;
+        final int[][] expected = new int[0][0];
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwise_1x1() {
+        final int n = 1;
         final int[][] expected = { { 1 } };
-        assertSquareMatricesEqual(actual, expected);
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
     }
 
     @Test
-    public void rotateExceptDiagonals_2x2() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(2));
+    public void rotateClockwiseExceptDiagonals_1x1() {
+        final int n = 1;
+        final int[][] expected = { { 1 } };
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwise_2x2() {
+        final int n = 2;
+        final int[][] expected = {
+            { 3, 1 },
+            { 4, 2 }
+        };
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwiseExceptDiagonals_2x2() {
+        final int n = 2;
         final int[][] expected = {
             { 1, 2 },
             { 3, 4 }
         };
-        assertSquareMatricesEqual(actual, expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
     }
 
     @Test
-    public void rotateExceptDiagonals_3x3() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(3));
+    public void rotateClockwise_3x3() {
+        final int n = 3;
+        final int[][] expected = {
+            { 7, 4, 1 },
+            { 8, 5, 2 },
+            { 9, 6, 3 }
+        };
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwiseExceptDiagonals_3x3() {
+        final int n = 3;
         final int[][] expected = {
             { 1, 4, 3 },
             { 8, 5, 2 },
             { 7, 6, 9 }
         };
-        assertSquareMatricesEqual(actual, expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
     }
 
     @Test
-    public void rotateExceptDiagonals_4x4() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(4));
+    public void rotateClockwise_4x4() {
+        final int n = 4;
+        final int[][] expected = {
+            { 13, 9, 5, 1 },
+            { 14, 10, 6, 2 },
+            { 15, 11, 7, 3 },
+            { 16, 12, 8, 4 } };
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwiseExceptDiagonals_4x4() {
+        final int n = 4;
         final int[][] expected = {
             { 1, 9, 5, 4 },
             { 14, 6, 7, 2 },
             { 15, 10, 11, 3 },
             { 13, 12, 8, 16 }
         };
-        assertSquareMatricesEqual(actual, expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
     }
 
     @Test
-    public void rotateExceptDiagonals_5x5() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(5));
+    public void rotateClockwise_5x5() {
+        final int n = 5;
+        final int[][] expected = {
+            { 21, 16, 11, 6, 1 },
+            { 22, 17, 12, 7, 2 },
+            { 23, 18, 13, 8, 3 },
+            { 24, 19, 14, 9, 4 },
+            { 25, 20, 15, 10, 5 }
+        };
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwiseExceptDiagonals_5x5() {
+        final int n = 5;
         final int[][] expected = {
             { 1, 16, 11, 6, 5 },
             { 22, 7, 12, 9, 2 },
@@ -65,12 +145,30 @@ public class RotateImageTest {
             { 24, 17, 14, 19, 4 },
             { 21, 20, 15, 10, 25 }
         };
-        assertSquareMatricesEqual(actual, expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
     }
 
     @Test
-    public void rotateExceptDiagonals_6x6() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(6));
+    public void rotateClockwise_6x6() {
+        final int n = 6;
+        final int[][] expected = {
+            { 31, 25, 19, 13, 7, 1 },
+            { 32, 26, 20, 14, 8, 2 },
+            { 33, 27, 21, 15, 9, 3 },
+            { 34, 28, 22, 16, 10, 4 },
+            { 35, 29, 23, 17, 11, 5 },
+            { 36, 30, 24, 18, 12, 6 }
+        };
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwiseExceptDiagonals_6x6() {
+        final int n = 6;
         final int[][] expected = {
             { 1, 25, 19, 13, 7, 6 },
             { 32, 8, 20, 14, 11, 2 },
@@ -79,12 +177,31 @@ public class RotateImageTest {
             { 35, 26, 23, 17, 29, 5 },
             { 31, 30, 24, 18, 12, 36 }
         };
-        assertSquareMatricesEqual(actual, expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
     }
 
     @Test
-    public void rotateExceptDiagonals_7x7() {
-        final int[][] actual = solution.rotateExceptDiagonals(buildSquareMatrix(7));
+    public void rotateClockwise_7x7() {
+        final int n = 7;
+        final int[][] expected = {
+            { 43, 36, 29, 22, 15, 8, 1 },
+            { 44, 37, 30, 23, 16, 9, 2 },
+            { 45, 38, 31, 24, 17, 10, 3 },
+            { 46, 39, 32, 25, 18, 11, 4 },
+            { 47, 40, 33, 26, 19, 12, 5 },
+            { 48, 41, 34, 27, 20, 13, 6 },
+            { 49, 42, 35, 28, 21, 14, 7 }
+        };
+        assertSquareMatricesEquals(solution.rotateClockwise(buildSquareMatrix(n)), expected);
+    }
+
+    @Test
+    public void rotateClockwiseExceptDiagonals_7x7() {
+        final int n = 7;
         final int[][] expected = {
             { 1, 36, 29, 22, 15, 8, 7 },
             { 44, 9, 30, 23, 16, 13, 2 },
@@ -94,20 +211,14 @@ public class RotateImageTest {
             { 48, 37, 34, 27, 20, 41, 6 },
             { 43, 42, 35, 28, 21, 14, 49 }
         };
-        assertSquareMatricesEqual(actual, expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithIndexing(buildSquareMatrix(n)),
+                expected);
+        assertSquareMatricesEquals(
+                solution.rotateClockwiseExceptDiagonalsWithLooping(buildSquareMatrix(n)), expected);
     }
 
-    private int[][] buildSquareMatrix(final int size) {
-        final int[][] matrix = new int[size][size];
-        for (int i = 0, value = 1; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = value++;
-            }
-        }
-        return matrix;
-    }
-
-    private void assertSquareMatricesEqual(final int[][] actual, final int[][] expected) {
+    private void assertSquareMatricesEquals(final int[][] actual, final int[][] expected) {
         if (expected.length == 0) {
             assertThat("Matrix must be empty", actual.length, is(0));
             return;
@@ -140,6 +251,16 @@ public class RotateImageTest {
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+    private int[][] buildSquareMatrix(final int size) {
+        final int[][] matrix = new int[size][size];
+        for (int i = 0, value = 1; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = value++;
+            }
+        }
+        return matrix;
     }
 
 }
