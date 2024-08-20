b0c6810de105d063cfa8fe3d307b842b1bdc6a6c
Phi Luu <philectron@users.noreply.github.com>
2024-08-19 19:15:53 -0700

    Use JUnit features for more concise test code

--- tests/com/philectron/algorithms/searching/SearchTestBase.java
+++ tests/com/philectron/algorithms/searching/SearchTestBase.java
@@ -5,6 +5,9 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 import com.google.common.primitives.Ints;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public abstract class SearchTestBase {
@@ -12,87 +15,75 @@ public abstract class SearchTestBase {
     private static final int UNIQUE_TARGET = 2;
     private static final int DUPLICATE_TARGET = 5;
     private static final int NON_TARGET = 10;
-    private static final int[] ARRAY =
-            { DUPLICATE_TARGET, UNIQUE_TARGET, 8, 6, 4, 3, 5, 7, 1, DUPLICATE_TARGET };
-    private static final int[] SORTED_ARRAY = ARRAY.clone();
-
-    static {
-        Arrays.sort(SORTED_ARRAY);
-    }
+    private static final List<Integer> LIST =
+            List.of(DUPLICATE_TARGET, UNIQUE_TARGET, 8, 6, 4, 3, 5, 7, 1, DUPLICATE_TARGET);
+    private static final List<Integer> SORTED_LIST = LIST.stream().sorted().toList();
 
     private final SearchingAlgorithm searcher;
     private final boolean isSortRequired;
+
+    private List<Integer> list;
+    private int target;
 
     SearchTestBase(SearchingAlgorithm searcher, boolean isSortRequired) {
         this.searcher = searcher;
         this.isSortRequired = isSortRequired;
     }
 
+    @AfterEach
+    public void searchAndAssert() {
+        int[] array = list.stream().mapToInt(Integer::intValue).toArray();
+        int[] originalArray = array.clone();
+
+        assertWithMessage("First index of target %s in array %s", target, Arrays.toString(array))
+                .that(searcher.findFirst(array, target))
+                .isEqualTo(Ints.indexOf(array, target));
+
+        assertWithMessage("Last index of target %s in array %s", target, Arrays.toString(array))
+                .that(searcher.findLast(array, target))
+                .isEqualTo(Ints.lastIndexOf(array, target));
+
+        assertWithMessage("Whether array %s contains target %s", Arrays.toString(array), target)
+                .that(searcher.contains(array, target))
+                .isEqualTo(Ints.contains(array, target));
+
+        assertThat(array).isEqualTo(originalArray); // searching should not mutate the array
+    }
+
     @Test
     public void search_emptyArray_returnsNotFound() {
-        assertNotFound(new int[0], 1);
+        list = Collections.emptyList();
+        target = 1;
     }
 
     @Test
-    public void search_singletonArray_returnsIndex() {
-        int[] array = new int[1];
-        assertFound(array, array[0]);
+    public void search_singletonArray_returnsSingleIndex() {
+        list = Collections.singletonList(1);
+        target = list.get(0);
     }
 
     @Test
-    public void search_nCopiesArray_returnsIndexes() {
-        final int target = 1;
-        int[] array = { target, target, target, target, target };
-        assertFound(array, target);
+    public void search_nCopiesArray() {
+        list = Collections.nCopies(3, target);
+        target = 1;
     }
 
     @Test
-    public void search_uniqueTarget_returnsIndex() {
-        assertFound(isSortRequired ? SORTED_ARRAY : ARRAY, UNIQUE_TARGET);
+    public void search_uniqueTarget_returnsSingleIndex() {
+        list = isSortRequired ? SORTED_LIST : LIST;
+        target = UNIQUE_TARGET;
     }
 
     @Test
-    public void search_duplicateTargets_returnsIndexes() {
-        assertFound(isSortRequired ? SORTED_ARRAY : ARRAY, DUPLICATE_TARGET);
+    public void search_duplicateTargets() {
+        list = isSortRequired ? SORTED_LIST : LIST;
+        target = DUPLICATE_TARGET;
     }
 
     @Test
     public void search_nonTarget_returnsNotFound() {
-        assertNotFound(isSortRequired ? SORTED_ARRAY : ARRAY, NON_TARGET);
-    }
-
-    private void assertFound(int[] array, int target) {
-        int[] originalArray = array.clone();
-        assertFirstIndex(array, target, Ints.indexOf(array, target));
-        assertLastIndex(array, target, Ints.lastIndexOf(array, target));
-        assertContains(array, target, true);
-        assertThat(array).isEqualTo(originalArray); // Searching should not mutate the array.
-    }
-
-    private void assertNotFound(int[] array, int target) {
-        int[] originalArray = array.clone();
-        assertFirstIndex(array, target, SearchingAlgorithm.INDEX_NOT_FOUND);
-        assertLastIndex(array, target, SearchingAlgorithm.INDEX_NOT_FOUND);
-        assertContains(array, target, false);
-        assertThat(array).isEqualTo(originalArray); // Searching should not mutate the array.
-    }
-
-    private void assertFirstIndex(int[] array, int target, int expectedIndex) {
-        assertWithMessage("First index of target %s in array %s", target, Arrays.toString(array))
-                .that(searcher.findFirst(array, target))
-                .isEqualTo(expectedIndex);
-    }
-
-    private void assertLastIndex(int[] array, int target, int expectedIndex) {
-        assertWithMessage("Last index of target %s in array %s", target, Arrays.toString(array))
-                .that(searcher.findLast(array, target))
-                .isEqualTo(expectedIndex);
-    }
-
-    private void assertContains(int[] array, int target, boolean expectedResult) {
-        assertWithMessage("Whether array %s contains target %s", Arrays.toString(array), target)
-                .that(searcher.contains(array, target))
-                .isEqualTo(expectedResult);
+        list = isSortRequired ? SORTED_LIST : LIST;
+        target = NON_TARGET;
     }
 
 }
--- tests/com/philectron/algorithms/sorting/SortTestBase.java
+++ tests/com/philectron/algorithms/sorting/SortTestBase.java
@@ -2,57 +2,53 @@ package com.philectron.algorithms.sorting;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public abstract class SortTestBase {
 
     private final SortingAlgorithm sorter;
 
+    private int[] array;
+
     SortTestBase(SortingAlgorithm sorter) {
         this.sorter = sorter;
     }
 
-    @Test
-    public void sort_emptyArray() {
-        int[] array = new int[0];
+    @AfterEach
+    public void sortAndAssert() {
         sorter.sort(array);
         assertThat(array).asList().isInOrder();
+    }
+
+    @Test
+    public void sort_emptyArray() {
+        array = new int[0];
     }
 
     @Test
     public void sort_singletonArray() {
-        int[] array = new int[1];
-        sorter.sort(array);
-        assertThat(array).asList().isInOrder();
+        array = new int[1];
     }
 
     @Test
     public void sort_nCopiesArray() {
-        int[] array = { 1, 1, 1, 1, 1 };
-        sorter.sort(array);
-        assertThat(array).asList().isInOrder();
+        array = new int[] { 1, 1, 1, 1, 1 };
     }
 
     @Test
     public void sort_ascendingArray() {
-        int[] array = { 1, 2, 3, 4, 5 };
-        sorter.sort(array);
-        assertThat(array).asList().isInOrder();
+        array = new int[] { 1, 2, 3, 4, 5 };
     }
 
     @Test
     public void sort_descendingArray() {
-        int[] array = { 5, 4, 3, 2, 1 };
-        sorter.sort(array);
-        assertThat(array).asList().isInOrder();
+        array = new int[] { 5, 4, 3, 2, 1 };
     }
 
     @Test
     public void sort_arbitraryArray() {
-        int[] array =
-                { 21, 37, 36, 19, 30, 27, 25, 36, 32, 28, 13, 33, 34, 20, 30, 4, 15, 40, 5, 10 };
-        sorter.sort(array);
-        assertThat(array).asList().isInOrder();
+        array = new int[] { 21, 37, 36, 19, 30, 27, 25, 36, 32, 28, 13, 33, 34, 20, 30, 4, 15, 40 };
     }
 
 }
